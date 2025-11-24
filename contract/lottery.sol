0c2714a3b21cf6a7478f408ed799be6a310d7de1
ckrzysp <pawluschristopher@gmail.com>
2025-11-23 19:33:44 -0500

    added basic data types for lottery setup, constructor added

--- contract/lottery.sol
+++ contract/lottery.sol
@@ -1,7 +1,25 @@
 // SPDX-License-Identifier: MIT
+// @custom:dev-run-script ./contract/lottery.sol
 pragma solidity ^0.8.0;
+import "hardhat/console.sol";
 
 /// Lottery with immutability.
 contract Lottery {
-    
+    /// Essential variables for the lottery
+    address[] participants;
+    address public host;
+    int64 public constant monetaryPrize = 5 * 10**5; 
+    int64 public ticketCost = 0.015 ether;
+
+    constructor() {
+        // User who deployed the contract
+        host = msg.sender;
+    }
+
+    /* Add functions
+    * entry purchase
+    * winner selection
+    * money transfer 
+    * - Christopher / Krzysio
+    */ 
 }
