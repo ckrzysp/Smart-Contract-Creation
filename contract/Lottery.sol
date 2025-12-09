71f8567b9bdfa003f96280145f27ffcf9bce6d12
lausan3 <lauanthony4455@gmail.com>
2025-12-08 22:55:12 -0500

    feat: revamp winner selection system by introducing function to find all players who have winning tickes

--- contract/Lottery.sol
+++ contract/Lottery.sol
@@ -24,20 +24,18 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+
 import "@chainlink/contracts/src/v0.8/vrf/dev/VRFV2PlusWrapperConsumerBase.sol";
 import "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+import "contract/Player.sol";
+import "utils/Utils.sol";
 
 /// Lottery with immutability.
 contract Lottery is VRFV2PlusWrapperConsumerBase {
-    /// Essential variables for the lottery
-    struct Player {
-        uint8[][5] ticketNumbers;
-        bool hasJoined;
-        uint16 numberOfTickets;
-    }
     // A dictionary of an address to the tickets it has purchased in the current iteration of the lottery.
-    mapping(address => Player) public participantsToTickets;
+    mapping(address => Player) public participantsToTickets = new mapping(address => Player);
     uint16 private totalTickets = 0;
+    address[] participantAddresses = new address[];
 
     address payable public host;
 
@@ -51,8 +49,6 @@ contract Lottery is VRFV2PlusWrapperConsumerBase {
     uint256 internal fee;
     uint256 public randomResult;
     uint256 public requestId;
-    // uint256 public min;
-    // uint256 public max;  don't think a max makes sense anymore
 
     constructor(
         uint256 min_,
@@ -61,8 +57,6 @@ contract Lottery is VRFV2PlusWrapperConsumerBase {
     ) VRFV2PlusWrapperConsumerBase(vrfWrapper) {
         // User who deployed the contract
         host = payable(msg.sender);
-        // min = min_;
-        // max = max_;
     }
 
     modifier onlyHost() {
@@ -70,22 +64,75 @@ contract Lottery is VRFV2PlusWrapperConsumerBase {
         _;
     }
 
-    function start() external onlyHost {
-        // require(
-        //     participants.length >= min,
-        //     "There are not enough people to start this lottery"
-        // );
-        require(participants.length > 0, "No participants");
+    // // Check that all 5 numbers are unique and in range 1 -> 99
+    function validateTicketNumbers(uint8[5] calldata ticketNumbers) internal pure {
+        for (uint256 i = 0; i < 5; i++) {
+            require(ticketNumbers[i] >= 1 && ticketNumbers[i] <= 99, "Out of range");
+            for (uint256 j = i + 1; j < 5; j++) {
+                require(ticketNumbers[i] != ticketNumbers[j], "Duplicate in ticket");
+            }
+        }
+    }
+
+    /*
+     *  joinLottery
+     *  Allows a user to join the lottery by buying a ticket
+     *  Adds them to lottery basket
+     */
+    function buyTicket(uint8[5] calldata ticketNumbers) external payable {
+        require(
+            msg.value < ticketCost, 
+            "Wrong amount of ether to join. Please pay at least 0.15 ether. Any more will be considered a tip."
+        );
+        validateTicketNumbers(ticketNumbers);
+
+        require(payable(msg.sender), "You have to be a payable address to be sent the lottery prize"); 
+
+        participantsToTickets[msg.sender].push(ticketNumbers);
+        participantsToTickets[msg.sender].numberOfTickets += 1;
+
+        if (participantsToTickets[msg.sender].hasJoined = false) {
+            participantsToTickets[msg.sender].hasJoined = true;
+
+            participantAddresses.push(msg.sender);
+        }
+
+        totalTickets += 1;
+
+        emit participantJoined(msg.sender, "joined");
+    }
+
+    function startLottery() external onlyHost {
+        // require(participants.length > 0, "No participants");
         
         // Added this line to check to make sure the contract has enough money to pay the winner
         // Prevent lottery from starting without funds
-        require(address(this).balance >= monetaryPrize, "Insufficient contract balance for prize.");
+        // require(address(this).balance >= monetaryPrize, "Insufficient contract balance for prize.");
+        uint256 totalPrizePool = totalTickets * (ticketCost * 0.8);
+        uint8[5] winningNumbers = [1, 2, 3, 4, 5];  // This is just an example
+        uint256 numPlayers = participantAddresses.length;
+        Player[] players = new Player[](numPlayers);
+
+        for (uint256 i = 0; i < numPlayers; i++) {
+            players.push(participantsToTickets[participantAddresses[i]]);
+        }
+
+        (address[] winners, uint256 amountWon) = determinePrizes(
+            totalPrizePool,
+            winningNumbers,
+            participantAddresses,
+            players
+        );
+
+        for (uint256 i = 0; i < winners.length; i++) {
+            // Send the money to each address
+        }
     
         // Request random number from Chainlink VRF v2.5 Direct Funding
-        bytes memory extraArgs = VRFV2PlusClient._argsToBytes(
-            VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
-        );
-        (requestId, ) = requestRandomness(100000, 3, 1, extraArgs);
+        // bytes memory extraArgs = VRFV2PlusClient._argsToBytes(
+        //     VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+        // );
+        // (requestId, ) = requestRandomness(100000, 3, 1, extraArgs);
     }
 
     /*  
@@ -128,34 +175,13 @@ contract Lottery is VRFV2PlusWrapperConsumerBase {
         amountToSend = 0;
     }
 
-    /*
-     *   getParticipant
-     *   Get User in lottery
-     *   Returns participant
-     */
-
-    /*
-    function getParticipant(address user) private view returns (address) {
-        address found = host;
-
-        for (uint64 i = 0; i < participants.length; i++) {
-            if (user == participants[i]) {
-                found = participants[i];
-                return found;
-            }
-        }
-
-        return found;
-    }
-    */
-
     /// Checks if a user is signed up for the lottery.
     /// Refactored from the original loop approach, which iterated over the entire participant array and would become inefficient as the number of participants grew.
     /// Now uses the `hasJoined` mapping, which provides a direct lookup for constant-time access regardless of how many people are in the lottery.
     /// @param user The address to check for participation.
     /// @return bool Returns true if the user has joined, false otherwise.
     function getParticipantStatus(address user) public view returns (bool) {
-        return participantsToTickets[user].hasJoined;
+        return participantsToTickets[user];
     }
 
     /// Replaced the for loop in getParticipant with a direct mapping lookup. The old method got slower as more participants joined, so this keeps it fast regardless of size.
---
+++ utils/Utils.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "contract/Player.sol";
+
+function determinePrizes(
+    uint256 totalPrizePool,
+    uint8[5] calldata winningNumbers,
+    address[] calldata addresses,
+    Player[] calldata players
+) pure returns (address[] memory winners, uint256 winAmount) {
+    require(addresses.length == players.length, "Length mismatch");
+
+    uint256 totalJackpotWinners = 0;
+
+    // find all jackpot winners
+    for (uint256 i = 0; i < players.length; i++) {  // for each player
+        uint256 n = players[i].numberOfTickets;
+
+        for (uint256 j = 0; j < n; j++) {  // for each ticket the current player has
+            uint8[5] calldata ticket = players[i].ticketNumbers[j];
+
+            bool isJackpotWinner = true;
+            for (uint8 x = 0; x < 5; x++) {
+                if (ticket[x] != winningNumbers[x]) {
+                    isJackpotWinner = false;
+                    break;
+                }
+            }
+
+            if (isJackpotWinner) {
+                totalJackpotWinners += 1;
+            }
+        }
+    }
+
+    if (totalJackpotWinners == 0) {
+        return (new address[](0), 0);
+    }
+    
+    // Allocate winners with a fixed size
+    winners = new address[](totalJackpotWinners);
+    uint256 winnerIndex = 0;
+    uint256 share = totalPrizePool / totalJackpotWinners;
+
+    // Populate winners
+    for (uint256 i = 0; i < players.length; i++) {
+        uint256 n = players[i].numberOfTickets;
+
+        for (uint256 j = 0; j < n; j++) {
+            bool isJackpotWinner = true;
+
+            for (uint256 x = 0; x < 5; x++) {
+                if (players[i].ticketNumbers[x][j] != winningNumbers[x]) {
+                    isJackpotWinner = false;
+                    break;
+                }
+            }
+
+            if (isJackpotWinner) {
+                winners[winnerIndex] = addresses[i];
+                winnerIndex++;
+            }
+        }
+    }
+
+    return (winners, share);
+}
